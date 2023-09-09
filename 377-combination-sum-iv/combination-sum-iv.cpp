de90de1f7ecb7cf600cc975e3917eb508fbbdc50
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2023-09-09 15:03:43 +0530

    Time: 15 ms (5.04%) | Memory: 6.6 MB (46.24%) - LeetSync

---
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int calc(vector<int>& nums, int target, vector<int>& DP)
+    {
+        cout<<target<<endl;
+         if(target==0)
+            return 1;
+         if(target<0)
+           return 0;
+         if(DP[target]!=-1)
+           return DP[target];     
+         else
+         {
+             int sum=0;
+             for(int i=0; i<nums.size(); i++)
+             {
+                 sum+=calc(nums, target-nums[i], DP);
+             }
+             return DP[target]=sum;
+         } 
+
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> DP(target+1, -1);
+          return calc(nums, target, DP);
+    }
+};
