9293b9fbacab5a3c3268ed782138e35f135f77e1
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2023-09-10 17:31:17 +0530

    Time: 1365 ms (20.00%) | Memory: 57.3 MB (100.00%) - LeetSync

---
+++ 1907-count-pairs-with-xor-in-a-range/count-pairs-with-xor-in-a-range.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int CountPairs(int[] nums, int low, int high) {
+        int n = nums.Length;
+
+        int count=0;
+         Array.Sort(nums);
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i+1; j<n; j++)
+            {
+                if(nums[j]-nums[i]>high)
+                   break;
+                int x = nums[i]^nums[j];
+                if(x>=low&&x<=high)
+                   count++;
+            }
+        }
+        return count;
+    }
+}
