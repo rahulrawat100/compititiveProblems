1a44d1f01d7cdeb3f55c62e3f8c0b84ab7923a42
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2023-09-12 10:34:29 +0530

    Time: 142 ms (52.44%) | Memory: 46.2 MB (45.45%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,43 +1,78 @@
 class Solution {
 public:
-    int calc(vector<vector<int>>& grid, int k, int i, int j, vector<vector<bool>>& trav, vector<vector<vector<int>>>& DP)
+    int calc(vector<vector<int>>& grid,queue<pair<pair<int, int>, int>>& Q, vector<vector<vector<bool>>>& trav)
     {
-        if(i<0 || j<0 || i==grid.size() || j==grid[0].size()  || trav[i][j])
-           return INT_MAX;
-         if((grid[i][j]==1&&k==0))
-           return INT_MAX-1;  
-      //  cout<<"try  "<<i<<"  "<<j<<endl;   
-        if(i==grid.size()-1&&j==grid[0].size()-1)
-           return 0;
-        if(DP[i][j][k]!=-1)
-           return DP[i][j][k];      
+        if(Q.size()==0 )
+          return INT_MAX;
         else
         {
-            int res= INT_MAX;
-            if(grid[i][j]==1)
-                k--;
-                trav[i][j]=true;
-                
-             res= min(res, calc(grid, k, i+1, j, trav, DP));
-             res= min(res, calc(grid, k, i-1, j, trav, DP));
-             res= min(res, calc(grid, k, i, j+1, trav, DP));
-             res= min(res, calc(grid, k, i, j-1, trav, DP));
-              trav[i][j]=false;
-              if(res!=INT_MAX)
-            // cout<<i<<"  "<<j<<"   "<<k<<"   "<<res+1<<endl;
-             if(res<INT_MAX)
-                DP[i][j][k]=res+1;
-              return res>=INT_MAX-1?res:res+1;
-        }   
-    }
+            int s= Q.size();
+            queue<pair<pair<int, int>, int>> trv;
+            while(s>0)
+            { 
+                int x = Q.front().first.first;
+                int y = Q.front().first.second;
+                int k =Q.front().second;
+                s--;
+                  Q.pop();
+                if(x<0 ||y<0 ||x==grid.size() || y==grid[0].size() ||  trav[x][y][k] )
+                  continue;
+                   trv.push({{x, y},k});
+                  if(x==grid.size()-1&&y==grid[0].size()-1)
+                    return 0;
 
+                  if(grid[x][y]==1)
+                  {
+                     if(k>0)
+                     {
+                      trav[x][y][k]=true; 
+                     Q.push({{x+1, y}, k-1});
+                     Q.push({{x-1, y}, k-1});
+                     Q.push({{x, y+1}, k-1});
+                     Q.push({{x, y-1}, k-1});
+                     }
+                  }
+                  else
+                  {
+                    trav[x][y][k]=true;
+                    Q.push({{x+1, y}, k});
+                     Q.push({{x-1, y}, k});
+                     Q.push({{x, y+1}, k});
+                     Q.push({{x, y-1}, k});
+                  }
+            } 
+            int res= calc(grid, Q, trav);
+            while(!trv.empty())
+            {
+                int x = trv.front().first.first;
+                int y = trv.front().first.second;
+                int k =trv.front().second;
+                trav[x][y][k]=false;
+                trv.pop();
+            }
+            return res==INT_MAX?res:res+1;
+        }  
+    }
     int shortestPath(vector<vector<int>>& grid, int k) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<vector<int>>> DP(n, vector<vector<int>>(m, vector<int>(k+1,-1)));
-        vector<vector<bool>> trav(n, vector<bool>(m));
-        int res= calc(grid, k, 0, 0, trav, DP);
-        if(res==37)return 21;
-        return res>=INT_MAX-1?-1:res;
+        queue<pair<pair<int, int>, int>> q;
+        vector<vector<vector<bool>>> trav(grid.size(), vector<vector<bool>>(grid[0].size(), vector<bool>(k+1)));
+        q.push({{0, 0}, k});
+        int x= calc(grid,  q, trav);
+        return x==INT_MAX?-1:x;
     }
 };
+/*
+[[0,0]
+,[1,0]
+,[1,0]
+,[1,0]
+,[1,0]
+,[1,0]
+,[0,0]
+,[0,1]
+,[0,1]
+,[0,1]
+,[0,0]
+,[1,0]
+,[1,0]
+,[0,0]]*/
