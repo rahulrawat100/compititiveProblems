fbd07c5369fac1634c9d07784dd13efb5dd19874
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2023-10-29 10:36:47 +0530

    Time: 1347 ms (22.22%) | Memory: 401 MB (11.11%) - LeetSync

---
+++ 3179-maximum-points-after-collecting-coins-from-all-nodes/maximum-points-after-collecting-coins-from-all-nodes.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int calc(vector<vector<int>>& adj, vector<int>& coins, int k, int i, vector<bool>& trav, int div, vector<unordered_map<int, int>>& DP)
+    {
+        int d=pow(2, div);
+        if(trav[i] || div>15)
+            return 0;
+        if(adj[i].size()==0)
+            return max(coins[i]/div-k, coins[i]/(2*d));
+        if(DP[i].find(div)!=DP[i].end())
+           return DP[i][div];    
+        else
+        {
+            trav[i]=true;
+            long long res1=0;
+            long long res2=0;
+            for(int j=0; j<adj[i].size(); j++)
+            {
+                res1+=calc(adj, coins, k, adj[i][j], trav, div, DP);
+                if(coins[i]/d-k<=coins[i]/(2*d))
+                res2+=calc(adj, coins, k, adj[i][j], trav, div+1, DP);
+            }
+            int r= max(coins[i]/d-k+res1, coins[i]/(2*d)+res2);
+           // cout<<i<<"  "<<r<<endl;
+            trav[i]=false;
+            return DP[i][div]=r;
+        }
+    }
+    int maximumPoints(vector<vector<int>>& edges, vector<int>& coins, int k) {
+        int n = coins.size();
+        vector<vector<int>> adj(n);
+        
+        for(int i=0; i<edges.size(); i++)
+        {
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+        }
+        vector<bool> trav(n);
+        vector<unordered_map<int, int>> DP(n);
+        return calc(adj, coins, k, 0, trav, 0, DP);
+        
+    }
+};
+
+    //     0
+
+    //  1    2   4
+
+    //        3   
