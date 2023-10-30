2f4003e10127291e31291a5a93f85630fb5f435c
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2023-10-30 09:04:30 +0530

    Time: 16 ms (13.23%) | Memory: 10.6 MB (45.81%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
+   static int bitter(int a)
+   {
+       int c=0;
+       while(a>0)
+       {
+          if(a%2==1)
+             c++;
+           a=a/2;  
+       }
+       return c;
+   }
+    static bool comp(int a, int b)
+    {
+         int c1 = bitter(a);
+         int c2 = bitter(b);
+
+         if(c1<c2)return true;
+         if(c1==c2)return a<b;
+         return false;
+    }
     vector<int> sortByBits(vector<int>& arr) {
+        
+        
+        
+        sort(arr.begin(), arr.end(), comp);
+        return arr;
         vector<pair<int, int>> D;
 
         int n = arr.size();
