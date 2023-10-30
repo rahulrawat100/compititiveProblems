a22748027cca876f03be4b81154b03184ef29b5d
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2023-10-30 09:04:54 +0530

    Time: 12 ms (25.10%) | Memory: 10.7 MB (45.81%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -21,31 +21,8 @@ public:
          return false;
     }
     vector<int> sortByBits(vector<int>& arr) {
-        
-        
-        
         sort(arr.begin(), arr.end(), comp);
         return arr;
-        vector<pair<int, int>> D;
-
-        int n = arr.size();
-
-        for(int i=0; i<n; i++)
-        {
-            int m = arr[i];
-            int c=0;
-            while(m>0)
-            {
-                if(m%2==1)c++;
-                m=m/2;
-            }
-            D.push_back({c, arr[i]});
-        }
-        sort(D.begin(), D.end());
-        vector<int> res(n);
-        for(int i=0; i<n; i++){
-            res[i]=D[i].second;
-        }
-        return res;
+       
     }
 };
