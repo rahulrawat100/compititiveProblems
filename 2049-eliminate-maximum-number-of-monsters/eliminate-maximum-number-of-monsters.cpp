9a27b2382c16ca21fca172b6b946f442b700c100
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2023-11-08 11:24:18 +0530

    Time: 152 ms (15.73%) | Memory: 85.4 MB (43.47%) - LeetSync

---
+++ 2049-eliminate-maximum-number-of-monsters/eliminate-maximum-number-of-monsters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+        vector<int> reach;
+
+        int n = dist.size();
+
+        for(int i=0; i<n; i++)
+        {
+            reach.push_back((dist[i]+speed[i]-1)/speed[i]);
+        }
+
+        sort(reach.begin(), reach.end());
+        cout<<reach[0]<<endl;
+        for(int i=1; i<n; i++)
+        {
+            cout<<reach[i]<<endl;
+            if(reach[i]<=i)
+               return i;
+        }
+        return n;
+    }
+};
