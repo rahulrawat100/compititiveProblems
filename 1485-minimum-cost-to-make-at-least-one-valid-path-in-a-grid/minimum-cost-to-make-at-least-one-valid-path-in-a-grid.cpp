8b1daebb9781f193da70e54526a0256aaf326237
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2024-01-05 15:28:23 +0530

    Time: 983 ms (5.06%) | Memory: 22.3 MB (41.69%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -27,15 +27,39 @@ public:
           }    
           return false;
     }
+
+    int binser(vector<vector<int>>& grid, int A, int B)
+    {
+        cout<<A<<"  "<<B<<endl;
+        int m = grid.size();
+        int n = grid[0].size();
+        if(A==B)
+          return A;
+        else
+        {
+            int mid = A+(B-A)/2;
+            vector<vector<int>> trav1(m, vector<int>(n, -1));
+            vector<vector<int>> trav2(m, vector<int>(n, -1));
+            if(valid(grid, 0, 0, mid, trav1))
+            {
+                if(!valid(grid, 0, 0, mid-1, trav2))
+                   return mid;
+                 else
+                  return binser(grid,A, mid-1);  
+            }
+            else
+            {
+                if(valid(grid, 0, 0, mid+1, trav2))
+                   return mid+1;
+                 else
+                  return binser(grid, mid+1, B);  
+            }
+            
+        }  
+    }
     int minCost(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        for(int i=0; i<=m+n; i++)
-        {
-             vector<vector<int>> trav(m, vector<int>(n, -1));
-            if(valid(grid, 0, 0, i, trav))
-               return i;
-        }   
-        return m+n;
+        return binser(grid, 0, m+n);
     }
 };
