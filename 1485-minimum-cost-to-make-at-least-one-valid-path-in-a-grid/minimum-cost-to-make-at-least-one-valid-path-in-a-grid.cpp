7e992b2067a565e416c2833dbb8dd6624ba00684
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2024-01-05 17:25:50 +0530

    Time: 150 ms (24.82%) | Memory: 19.7 MB (52.05%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,68 +1,43 @@
 class Solution {
 public:
-    bool valid(vector<vector<int>>& grid, int x, int y, int k, vector<vector<int>>& trav)
+    int m;
+    int n;
+    int calc(vector<vector<int>>& grid, priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>>& pq, vector<vector<int>>& trav)
     {
-        int m = grid.size();
-        int n = grid[0].size();
-       // cout<<"**"<<x<<"  "<<y<<endl;
-        if(k<0 || x<0 || y<0 || x==m || y==n ||k<=trav[x][y])
-           return false;
-        //cout<<x<<"  "<<y<<endl;   
-        if(x==m-1 && y==n-1)
+        int curr=pq.top().first;
+        while(pq.size()>0&&pq.top().first==curr)
         {
-            //cout<<x<<"  "<<y<<"  "<<k<<endl;
-          return true;
+           // cout<<pq.size()<<endl;;
+            int x = pq.top().second.first;
+            int y = pq.top().second.second;
+            int z = pq.top().first;
+           // cout<<x<<"  "<<y<<endl;
+             pq.pop();
+          //   cout<<trav.size()<<"  "<<trav[0].size()<<endl;
+            if(x<0 || y<0 || x==m || y==n || trav[x][y]==1)
+              continue;
+             if(x==m-1&&y==n-1)
+                return z; 
+            //  cout<<x<<" "<<y<<"  "<<z<<endl;  
+              vector<int> D(4, 1);
+              D[grid[x][y]-1]--;
+             trav[x][y]=1;
+            pq.push({curr+D[0],{x, y+1}});
+            pq.push({curr+D[1],{x, y-1}});
+            pq.push({curr+D[2],{x+1, y}});
+            pq.push({curr+D[3],{x-1, y}});  
+    
         }
-         else
-          {
-              trav[x][y]=k;
-              if(grid[x][y]==1)
-                  return valid(grid, x, y+1, k, trav) || valid(grid, x, y-1, k-1, trav) || valid(grid, x+1, y, k-1, trav) || valid(grid, x-1, y, k-1, trav);
-              else if(grid[x][y]==2)
-                  return valid(grid, x, y+1, k-1, trav) || valid(grid, x, y-1, k, trav) || valid(grid, x+1, y, k-1, trav) || valid(grid, x-1, y, k-1, trav); 
-             else if(grid[x][y]==3)
-                  return valid(grid, x, y+1, k-1, trav) || valid(grid, x, y-1, k-1, trav) || valid(grid, x+1, y, k, trav) || valid(grid, x-1, y, k-1, trav); 
-             else if(grid[x][y]==4)
-                  return valid(grid, x, y+1, k-1, trav) || valid(grid, x, y-1, k-1, trav) || valid(grid, x+1, y, k-1, trav) || valid(grid, x-1, y, k, trav);             
-          }    
-          return false;
+                   // cout<<"**"<<endl;
+        return calc(grid, pq, trav);
     }
 
-    int binser(vector<vector<int>>& grid, int A, int B, vector<vector<int>>& trav)
-    {
-       // cout<<A<<"  "<<B<<endl;
-        int m = grid.size();
-        int n = grid[0].size();
-        if(A==B)
-          return A;
-        else
-        {
-            int mid = A+(B-A)/2;
-            vector<int> D(n, -1);
-            fill(trav.begin(), trav.end(), D);
-            if(valid(grid, 0, 0, mid, trav))
-            {
-                fill(trav.begin(), trav.end(), D);
-                if(!valid(grid, 0, 0, mid-1, trav))
-                   return mid;
-                 else
-                  return binser(grid,A, mid-1, trav);  
-            }
-            else
-            {
-                fill(trav.begin(), trav.end(), D);
-                if(valid(grid, 0, 0, mid+1, trav))
-                   return mid+1;
-                 else
-                  return binser(grid, mid+1, B, trav);  
-            }
-            
-        }  
-    }
     int minCost(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>> trav(m, vector<int>(n, -1));
-        return binser(grid, 0, m+n, trav);
+        m=grid.size();
+        n=grid[0].size();
+       priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+       pq.push({0, {0, 0}});
+       vector<vector<int>> trav(m, vector<int>(n, -1));
+       return calc(grid, pq, trav);
     }
 };
