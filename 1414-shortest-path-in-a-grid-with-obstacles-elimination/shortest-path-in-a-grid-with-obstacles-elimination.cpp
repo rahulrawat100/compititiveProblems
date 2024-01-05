2838bbc3bf4ad0f78778419150f303452284078c
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2024-01-05 13:08:43 +0530

    Time: 38 ms (75.47%) | Memory: 23.6 MB (58.02%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,68 +1,60 @@
 class Solution {
 public:
-    int calc(vector<vector<int>>& grid,queue<pair<pair<int, int>, int>>& Q, vector<vector<int>>& trav)
+    int calc(vector<vector<int>>& grid, queue<pair<int, pair<int, int>>> q1, vector<vector<int>>& trav)
     {
-        if(Q.size()==0 )
+        cout<<"***"<<endl;
+        int m = grid.size();
+        int n= grid[0].size();
+        if(q1.size()==0)
           return INT_MAX;
         else
         {
-            int s= Q.size();
-            while(s>0)
-            { 
-                int x = Q.front().first.first;
-                int y = Q.front().first.second;
-                int k =Q.front().second;
-                s--;
-                  Q.pop();
-                if(x<0 ||y<0 ||x==grid.size() || y==grid[0].size() ||  k<=trav[x][y] )
-                  continue;
-                  if(x==grid.size()-1&&y==grid[0].size()-1)
-                    return 0;
+            int s= q1.size();
 
-                  if(grid[x][y]==1)
-                  {
+            while(s>0)
+            {
+                int x = q1.front().first;
+                int y = q1.front().second.first;
+                int k = q1.front().second.second;
+                //cout<<x<<"  "<<y<<"  "<<k<<endl;
+                 q1.pop();
+                 s--;
+                if(x<0 || y<0 || x ==m || y==n || k<=trav[x][y])
+                  continue;
+                if(x==m-1&&y==n-1)
+                  return 0;  
+                trav[x][y]=k;
+                 if(grid[x][y]==1)
+                 {
                      if(k>0)
                      {
-                      trav[x][y]=k; 
-                     Q.push({{x+1, y}, k-1});
-                     Q.push({{x-1, y}, k-1});
-                     Q.push({{x, y+1}, k-1});
-                     Q.push({{x, y-1}, k-1});
+                         q1.push({x+1, {y, k-1}});
+                         q1.push({x-1, {y, k-1}});
+                         q1.push({x, {y+1, k-1}});
+                         q1.push({x, {y-1, k-1}});
                      }
-                  }
-                  else
-                  {
-                    trav[x][y]=k;
-                    Q.push({{x+1, y}, k});
-                     Q.push({{x-1, y}, k});
-                     Q.push({{x, y+1}, k});
-                     Q.push({{x, y-1}, k});
-                  }
-            } 
-            int res= calc(grid, Q, trav);
+                 } 
+                 else
+                 {
+                         q1.push({x+1, {y, k}});
+                         q1.push({x-1, {y, k}});
+                         q1.push({x, {y+1, k}});
+                         q1.push({x, {y-1, k}});
+                 }
+            }
+
+            int res=calc(grid, q1, trav);
             return res==INT_MAX?res:res+1;
         }  
     }
     int shortestPath(vector<vector<int>>& grid, int k) {
-        queue<pair<pair<int, int>, int>> q;
-        vector<vector<int>> trav(grid.size(), vector<int>(grid[0].size(), -1));
-        q.push({{0, 0}, k});
-        int x= calc(grid,  q, trav);
-        return x==INT_MAX?-1:x;
+        int m = grid.size();
+        int n= grid[0].size();
+         queue<pair<int, pair<int, int>>> q1;
+         q1.push({0, {0, k}});
+        vector<vector<int>> trav(m, vector<int>(n, -1));
+                    int res=calc(grid, q1, trav);
+            return res==INT_MAX?-1:res;
+
     }
 };
-/*
-[[0,0]
-,[1,0]
-,[1,0]
-,[1,0]
-,[1,0]
-,[1,0]
-,[0,0]
-,[0,1]
-,[0,1]
-,[0,1]
-,[0,0]
-,[1,0]
-,[1,0]
-,[0,0]]*/
