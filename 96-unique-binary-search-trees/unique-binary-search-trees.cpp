90cfd76de9ec22578bb42bc11095bc97587dea7f
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2024-01-07 02:58:15 +0530

    Time: 0 ms (100.00%) | Memory: 6.9 MB (5.51%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    int calc(int i, int j)
+    int calc(int i, int j, vector<vector<int>>& DP)
     {
         if(i>=j)
            return 1;
+        if(DP[i][j]!=-1)
+           return DP[i][j];   
         else
           {
               int res=0;
               for(int k=i; k<=j; k++)
               {
-                 res+=calc(i, k-1)*calc(k+1, j);
+                 res+=calc(i, k-1, DP)*calc(k+1, j, DP);
               }
-              return res;
+              return DP[i][j]=res;
           }   
     }
     int numTrees(int n) {
-        return calc(1, n);
+        vector<vector<int>> DP(n+1, vector<int>(n+1, -1));
+        return calc(1, n, DP);
     }
 };
