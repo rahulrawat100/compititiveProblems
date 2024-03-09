76e1492e877fddfb5189586d137d58b1522ec23b
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2024-03-09 20:58:47 +0530

    Time: 3 ms (71.79%) | Memory: 12.1 MB (7.80%) - LeetSync

---
+++ 936-rle-iterator/rle-iterator.cpp
@@ -0,0 +1,31 @@
+class RLEIterator {
+public:
+    map<long long, int> D;
+    long long curr=0;
+    long long count=0;
+    RLEIterator(vector<int>& encoding) {
+        int n = encoding.size();
+        for(int i=0; i<n; i+=2)
+        {
+            int freq= encoding[i];
+            if(freq==0)continue;
+            count+=freq;
+            D[count]=encoding[i+1];
+        }
+    }
+    
+    int next(int n) {
+        if(curr>count)return -1;
+        curr+=n;
+        //if(curr>count)return -1;
+        auto it = D.lower_bound(curr);
+        if(it==D.end())return -1;
+        return it->second;
+    }
+};
+
+/**
+ * Your RLEIterator object will be instantiated and called as such:
+ * RLEIterator* obj = new RLEIterator(encoding);
+ * int param_1 = obj->next(n);
+ */
