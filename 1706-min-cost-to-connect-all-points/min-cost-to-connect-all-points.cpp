deec659e9612b8a19c455b66d52cc0b4747e686e
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2024-04-10 18:50:35 +0530

    Time: 106 ms (82.14%) | Memory: 61.6 MB (71.82%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,35 +1,47 @@
+
 class Solution {
 public:
+    int getpar(vector<int>& par, int x)
+    {
+        if(par[x]==x)
+           return x;
+        else
+           return par[x]=getpar(par, par[x]);   
+    }
     int minCostConnectPoints(vector<vector<int>>& points) {
-          int n = points.size();
-          priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-          int count=1;
-          vector<bool> trav(n);
-          trav[0]=true;
-          int curr=0;
-          int cost=0;
-          while(count<n)
-          {
-              for(int j=0; j<n; j++)
-              {
-                  if(trav[j])continue;
-                  int cost = abs(points[curr][0]-points[j][0])+abs(points[curr][1]-points[j][1]);
-                  pq.push({cost, {curr, j}});
-              }
+        priority_queue<pair<int, pair<int, int>>> pq;
 
-              while(trav[pq.top().second.second])
-              {
-                  pq.pop();
-              }
-              cost+=pq.top().first;
-              curr=pq.top().second.second;
-              pq.pop();
-              trav[curr]=true;
-              count++;
-          }
-          return cost;
-          
-          
-          
+        int n = points.size();
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i+1; j<n; j++)
+            {
+                int dist = abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
+                pq.push({-dist, {i, j}});
+            }
+        }
+        vector<int> par(n);
+        for(int i=0; i<n; i++)
+        {
+            par[i]=i;
+        }
+        int count=1;
+        int res=0;
+        while(count<n&&!pq.empty())
+        {
+            int i = pq.top().second.first;
+            int j = pq.top().second.second;
+            int I = getpar(par, i);
+            int J = getpar(par, j);
+            if(I!=J)
+            {
+                par[I]=J;
+                count++;
+                res-=pq.top().first;
+            }
+            pq.pop();
+        }
+        return res;
     }
 };
