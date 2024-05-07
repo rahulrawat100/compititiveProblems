aeddea1b4b4bd35a24684c9f81f7313eb2ab472c
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2024-05-07 18:02:41 +0530

    Time: 279 ms (17.12%) | Memory: 195.7 MB (5.94%) - LeetSync

---
+++ 3395-minimum-length-of-anagram-concatenation/minimum-length-of-anagram-concatenation.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool check(vector<vector<int>>& D, int len)
+    {
+        int prev=0;
+        int n=D.size();
+        for(int i=len; i<D.size(); i+=len)
+        {
+            for(int j=0; j<26; j++)
+            {
+                int f=D[i][j]-D[prev][j];
+                int t=D[n-1][j];
+                if(f==0&&t==0)
+                   continue;
+                if(f==0&&t!=0)return false;   
+                if(t%f!=0 ||t/f!=(n-1)/len)
+                  return false;
+            }
+            prev=i;
+        }
+        return true;
+
+    }
+    int minAnagramLength(string s) {
+        int n=s.size();
+        vector<vector<int>> DP(n+1, vector<int>(26, 0));
+        for(int i=0; i<n; i++)
+        {
+            int ind = s[i]-'a';
+            for(int j=0; j<26; j++)
+            {
+                DP[i+1][j]=DP[i][j];
+            }
+            DP[i+1][ind]=DP[i][ind]+1;
+        }
+        vector<int> freq(26, 0);
+        for(int i=1; i<=n; i++)
+        {
+           if(check(DP, i))
+             return i;
+        }
+        return n;
+    }
+};
