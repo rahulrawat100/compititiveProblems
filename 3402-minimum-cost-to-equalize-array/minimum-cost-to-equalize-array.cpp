9f0bacdf7024881d52d0b23b8231de0b6cf1d342
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2024-05-16 19:35:35 +0530

    Time: 715 ms (16.02%) | Memory: 93.7 MB (20.68%) - LeetSync

---
+++ 3402-minimum-cost-to-equalize-array/minimum-cost-to-equalize-array.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    long long cost(vector<int>& nums, long long cost1, long long cost2, long long target, long long sum)
+    {
+        int n = nums.size();
+        long long diffs = n*target-sum;
+        if(2*cost1<=cost2)
+           return diffs*cost1;
+        int maxdiff = target-nums[0];
+                      long long k= diffs-maxdiff;
+        if(maxdiff>k)
+           {
+              return (k*cost2+(maxdiff-k)*cost1);
+           }
+        if(diffs%2==0)
+           return (cost2*(diffs/2));
+        else
+           return (cost2*(diffs/2)+cost1);      
+    }
+    int minCostToEqualizeArray(vector<int>& nums, int cost1, int cost2) {
+        int n = nums.size();
+        int mod = pow(10, 9)+7;
+        sort(nums.begin(), nums.end());
+        long long sum=0;
+        if(nums[0]==nums[n-1])return 0;
+        for(int i=0; i<n; i++)
+        {
+            sum+=nums[i];
+        }
+        long long res=LONG_MAX;
+        for(int i=nums[n-1]; i<=4*nums[n-1]; i++)
+        {
+            res=min(res, cost(nums, cost1, cost2, i, sum));
+        }
+        cout<<res<<endl;
+        return res%mod;
+    }
+};
