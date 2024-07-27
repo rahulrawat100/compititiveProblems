995bd54d735af7b4a4c1dffcf9516a402ad3db29
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2024-07-27 19:51:26 +0530

    Time: 202 ms (35.97%) | Memory: 98.8 MB (54.63%) - LeetSync

---
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+        vector<vector<long long>> D(26, vector<long long>(26, INT_MAX));
+
+        for(int i=0; i<26; i++)
+        {
+            D[i][i]=0;
+        }
+
+        int m =original.size();
+
+        for(int i=0; i<m; i++)
+        {
+           int u = original[i]-'a';
+           int v = changed[i]-'a';
+           long long w = cost[i];
+           if(u!=v)
+           D[u][v]=min(D[u][v],w);
+        }
+
+        for(int i=0; i<26; i++)
+        {
+            for(int j=0; j<26; j++)
+            {
+                for(int k=0; k<26; k++)
+                {
+                    if(j==k)continue;
+                    D[j][k]=min(D[j][k], D[j][i]+D[i][k]);
+                }
+            }
+        }
+
+        long long res=0;
+
+        for(int i=0; i<source.size(); i++)
+        {
+            int u = source[i]-'a';
+            int v = target[i]-'a';
+            //cout<<u<<"  "<<v<<"  "<<D[u][v]<<endl;
+            if(D[u][v]==INT_MAX)return -1;
+            res+=D[u][v];
+        }
+        return res;
+    }
+};
