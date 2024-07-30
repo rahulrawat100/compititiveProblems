25c7bde80913b43a93ecb496f157af355e71b8d6
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2024-07-30 15:34:31 +0530

    Time: 27 ms (78.04%) | Memory: 22.9 MB (41.39%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -1,69 +1,78 @@
 class Solution {
 public:
-    void checkPac(vector<vector<int>>& heights, int i, int j,int prev, vector<vector<int>>& DP)
+    void Add(vector<vector<int>>& heights,queue<pair<int, int>>& q, int i, int j, int prev, vector<vector<bool>>& trav)
     {
-        cout<<i<<"  "<<j<<endl;
         int m = heights.size();
         int n = heights[0].size();
-        if(i<0 || j<0 || i>=m || j>=n || heights[i][j]<prev|| DP[i][j]==1)
-           return ; 
-        else
-          {
-              DP[i][j]=1;
-              checkPac(heights, i+1, j, heights[i][j], DP);
-              checkPac(heights, i-1, j, heights[i][j], DP);
-              checkPac(heights, i, j+1, heights[i][j], DP);
-              checkPac(heights, i, j-1, heights[i][j], DP);      
-          }  
-    }
 
+        if(i<0 || j<0 || i==m || j==n || trav[i][j] || heights[i][j]<prev)
+           return;
+        trav[i][j]=true;   
+        q.push({i, j});
+    }
+    void possP(vector<vector<int>>& heights, queue<pair<int, int>>& q, vector<vector<bool>>& trav, vector<vector<int>>& pac)
+    {
+          while(!q.empty())
+          {
+              pair<int, int> p = q.front();
+              q.pop();
+              int i = p.first;
+              int j = p.second;
+              pac[i][j]+=1;
+              Add(heights, q, i+1, j, heights[i][j], trav);
+              Add(heights, q, i-1, j, heights[i][j], trav);
+              Add(heights, q, i, j+1, heights[i][j], trav);
+              Add(heights, q, i, j-1, heights[i][j], trav);
+          }
+    }
 
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         int m = heights.size();
         int n = heights[0].size();
-        vector<vector<int>> res;
-                        vector<vector<int>> DP1(m, vector<int>(n, -1));
-                 vector<vector<int>> DP2(m, vector<int>(n, -1));
-                 for(int i=0; i<m; i++)
-                 {
-                     for(int j=0;j<n; j++)
-                     {
-                         if(i==0||j==0)
-                           DP1[i][j]=1;
-                         if(i==m-1||j==n-1)
-                           DP2[i][j]=1;  
-                     }
-                 }
-        for(int i=0; i<m; i++)
+        queue<pair<int, int>> q;
+        vector<vector<bool>> trav(m, vector<bool>(n));
+        vector<vector<bool>> trav1(m, vector<bool>(n));
+        vector<vector<int>> poss(m, vector<int>(n, 0));
+        for(int i=0; i<n; i++)
         {
-            for(int j=0; j<n; j++)
-            {
-                if(DP1[i][j]==1)
-                {
-                cout<<"Pac"<<"  "<<i<<"  "<<j<<endl;
-                 checkPac(heights, i+1, j, heights[i][j], DP1);
-                 checkPac(heights, i-1, j, heights[i][j], DP1);
-                 checkPac(heights, i, j+1, heights[i][j], DP1);
-                 checkPac(heights, i, j-1, heights[i][j], DP1);
-                }
-                if(DP2[i][j]==1)
-                {
-                 cout<<"Atl"<<"  "<<i<<"  "<<j<<endl;
-                 checkPac(heights, i+1, j, heights[i][j], DP2);
-                 checkPac(heights, i-1, j, heights[i][j], DP2);
-                 checkPac(heights, i, j+1, heights[i][j], DP2);
-                 checkPac(heights, i, j-1, heights[i][j], DP2);
-                }
-            }
+           q.push({0, i});
+           trav[0][i]=true;
         }
+        for(int i=1; i<m; i++)
+        {
+           q.push({i, 0});
+           trav[i][0]=true;
+        }
+        possP(heights, q, trav, poss);
+        while(!q.empty())
+        {
+            q.pop();
+        }
+        for(int i=0; i<n; i++)
+        {
+           q.push({m-1, i});
+           trav1[m-1][i]=true;
+        }
+        for(int i=0; i<m-1; i++)
+        {
+           q.push({i, n-1});
+           trav1[i][n-1]=true;
+        }
+        possP(heights, q, trav1, poss);
+        vector<vector<int>> res;
+
         for(int i=0; i<m; i++)
         {
             for(int j=0; j<n; j++)
             {
-                 if(DP1[i][j]==1&&DP2[i][j]==1)
-                   res.push_back({i,j});
+                if(poss[i][j]==2)
+                  res.push_back({i, j});
             }
         }
         return res;
+
+
+
+
     }
 };
