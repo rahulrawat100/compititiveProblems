b6bc79bafb8b669e9b4212fa598dc3c4ebe74fa7
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2024-07-30 13:20:47 +0530

    Time: 70 ms (45.04%) | Memory: 29.3 MB (38.73%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -13,18 +13,27 @@ public:
         {
          if(i>0&&nums[i]==nums[i-1])
                 continue;
-            for(int j=i+1; j<n; j++)
-            {
-                if(j>i+1&&nums[j]==nums[j-1])
-                    continue;
-                int st = -(nums[i]+nums[j]);
-                if(st+nums[n-1]<0)
-                    break;
-                if((D.find(st)!=D.end()) &&(D[st]>j))
-                {
-                 ans.push_back({nums[i], nums[j], st});   
-                }
-            }
+           int j = i+1;
+           int k =n-1;
+           while(j<k)
+           {
+               int sum = nums[i]+nums[j]+nums[k];
+
+               if(sum==0)
+               {
+                   ans.push_back({nums[i], nums[j], nums[k]});
+                   j++;
+                   k--;
+                   while(j<k&&nums[j]==nums[j-1])
+                       j++;
+                   while(k>j&&nums[k]==nums[k+1])
+                       k--;;    
+               }
+               else if(sum<0)
+                 j++;
+               else
+                 k--;  
+           }
         }
         return ans;
         
