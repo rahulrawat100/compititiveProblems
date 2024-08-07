9f6959af0bfc01b2252bcc83fb63de3d746b3a99
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2024-08-07 22:36:03 +0530

    Time: 438 ms (54.98%) | Memory: 105.7 MB (94.02%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,42 +1,42 @@
 class Solution {
 public:
-    void Add(int x, int y, priority_queue<pair<int, pair<int, int>>>& pq, vector<vector<bool>>& trav, vector<vector<int>>& grid, int curr)
-    {
-                int m = grid.size();
-        int n = grid[0].size();
-       if(x<0 || y< 0 || x == m || y==n || trav[x][y])
-          return;
-          trav[x][y]=true;
-          int ob = grid[x][y]==1?curr-1:curr;
-          pq.push({ob, {x, y}});
-    }
-    int calc(vector<vector<int>>& grid, priority_queue<pair<int, pair<int, int>>>& pq, vector<vector<bool>>& trav)
+    void Add( vector<vector<int>>& grid, int i, int j, priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>>& pq, int x)
     {
         int m = grid.size();
-        int n = grid[0].size();
-
-        int curr=pq.top().first;
-
-        while(pq.top().first==curr)
+       int n = grid[0].size();
+        if(i<0 || j<0 || i==m || j==n || grid[i][j]==-1)
+           return ;
+        if(grid[i][j]==1)
+           pq.push({x+1, {i, j}});
+        else
+          pq.push({x, {i, j}});
+          grid[i][j]=-1;      
+    }
+    int calc(priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>>& pq, vector<vector<int>>& grid)
+    {
+        int x = pq.top().first;
+       int m = grid.size();
+       int n = grid[0].size();
+        while(pq.top().first==x)
         {
-            int x = pq.top().second.first;
-            int y = pq.top().second.second;
+            int i = pq.top().second.first;
+            int j = pq.top().second.second;
+
             pq.pop();
-            if(x==m-1&& y==n-1)
-               return -curr;
-            Add(x+1, y, pq, trav, grid, curr);
-            Add(x-1, y, pq, trav, grid, curr);
-            Add(x, y+1, pq, trav, grid, curr);
-            Add(x, y-1, pq, trav, grid, curr);
+
+            if(i==m-1&&j==n-1)
+               return 0;
+            Add(grid, i+1, j, pq,  x);
+            Add(grid, i-1, j, pq,  x);
+            Add(grid, i, j+1, pq,  x);
+            Add(grid, i, j-1, pq,  x);   
+
         }
-        return calc(grid, pq, trav);
+        return 1+calc(pq, grid);
     }
     int minimumObstacles(vector<vector<int>>& grid) {
-                int m = grid.size();
-        int n = grid[0].size();
-        priority_queue<pair<int, pair<int, int>>> pq;
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq ;
         pq.push({0, {0, 0}});
-        vector<vector<bool>> trav(m, vector<bool>(n));
-        return calc(grid, pq, trav);
+        return calc(pq, grid);
     }
 };
