280baa0dccb0e4507bdaa185d9becb3f4676af96
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2024-08-08 10:29:27 +0530

    Time: 1279 ms (11.01%) | Memory: 109.4 MB (53.59%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,45 +1,44 @@
 class Solution {
 public:
+    int getpar(vector<int>& par, int x)
+    {
+        if(par[x]==x)
+           return x;
+        else
+           return getpar(par, par[x]);   
+    }
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
-        vector<vector<pair<int, int>>> adj(n);
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int,  pair<int, int>>>, greater<pair<int,  pair<int, int>>>> pq;
         for(int i=0; i<n; i++)
         {
             for(int j=0;j<n; j++)
             {
                 if(i==j)continue;
                 int cost = abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
-                adj[i].push_back({cost, j});
+                pq.push({cost, {i,j}});
             }
         }
 
-        vector<bool> trav(n);
-
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-        for(int i=0; i<adj[0].size(); i++)
+        vector<int> par(n);
+        for(int i=0; i<n; i++)
         {
-            pq.push(adj[0][i]);
+            par[i]=i;
         }
-        trav[0]=true;
 
         int count=1;
         int cost=0;
-        while(count<n)
+        while(!pq.empty())
         {
             int c = pq.top().first;
-            int j= pq.top().second;
+            int j= pq.top().second.second;
+            int i =pq.top().second.first;
             pq.pop();
-            if(trav[j])
-              continue;
-            cost+=c;
-                        //cout<<c<<"  "<<j<<endl;
-           for(int i=0; i<adj[j].size(); i++)
-          {
-               pq.push(adj[j][i]);
-          }
-           trav[j]=true; 
-           count++;   
+            int I = getpar(par, i);
+            int J = getpar(par, j);
+            if(I==J)continue;
+            par[I]=J;
+            cost+=c; 
         }
         return cost;
     }
