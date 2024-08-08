05881c6e90b6814e8e60c5d28076327752973565
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2024-08-08 10:13:31 +0530

    Time: 196 ms (73.27%) | Memory: 165.7 MB (28.57%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,47 +1,46 @@
-
 class Solution {
 public:
-    int getpar(vector<int>& par, int x)
-    {
-        if(par[x]==x)
-           return x;
-        else
-           return par[x]=getpar(par, par[x]);   
-    }
     int minCostConnectPoints(vector<vector<int>>& points) {
-        priority_queue<pair<int, pair<int, int>>> pq;
-
         int n = points.size();
+        vector<vector<pair<int, int>>> adj(n);
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0;j<n; j++)
+            {
+                if(i==j)continue;
+                int cost = abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
+                adj[i].push_back({cost, j});
+            }
+        }
 
-        for(int i=0; i<n; i++)
+        vector<bool> trav(n);
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+        for(int i=0; i<adj[0].size(); i++)
         {
-            for(int j=i+1; j<n; j++)
-            {
-                int dist = abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
-                pq.push({-dist, {i, j}});
-            }
+            pq.push(adj[0][i]);
         }
-        vector<int> par(n);
-        for(int i=0; i<n; i++)
-        {
-            par[i]=i;
-        }
+        trav[0]=true;
+
         int count=1;
-        int res=0;
-        while(count<n&&!pq.empty())
+        int cost=0;
+        while(count<n)
         {
-            int i = pq.top().second.first;
-            int j = pq.top().second.second;
-            int I = getpar(par, i);
-            int J = getpar(par, j);
-            if(I!=J)
-            {
-                par[I]=J;
-                count++;
-                res-=pq.top().first;
-            }
+            int c = pq.top().first;
+            int j= pq.top().second;
             pq.pop();
+            if(trav[j])
+              continue;
+            cost+=c;
+                        //cout<<c<<"  "<<j<<endl;
+           for(int i=0; i<adj[j].size(); i++)
+          {
+               pq.push(adj[j][i]);
+          }
+           trav[j]=true; 
+           count++;   
         }
-        return res;
+        return cost;
     }
 };
