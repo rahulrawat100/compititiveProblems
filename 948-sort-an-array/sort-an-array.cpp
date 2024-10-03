a773e69d1a969194e991e8c7521afb6160eeba18
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2024-10-03 08:49:40 +0530

    Time: 111 ms (75.42%) | Memory: 69.2 MB (94.03%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,38 +1,41 @@
 class Solution {
 public:
-    void heapify(vector<int>& nums, int i, int ignore)
+    void heapify(vector<int>& nums, int i, int leave)
     {
-        int c1 = 2*i+1;
-        int c2 = 2*i+2;
-        int maxi =i;
-        //cout<<c1<<"  "<<c2<<endl;
-        if(c1<(nums.size()-ignore)&&nums[c1]>nums[maxi])
-           maxi = c1;
-        if(c2<(nums.size()-ignore)&&nums[c2]>nums[maxi])
-           maxi = c2;
-        if(maxi!=i)
-        {   
-        int t= nums[maxi];
-        nums[maxi]=nums[i];
-        nums[i] = t;
-        heapify(nums, maxi, ignore);
+        int left = 2*i+1;
+        int right= 2*i+2;
+        int n = nums.size();
+        int min_ind = i;
+        if(left<n-leave&&nums[left]>nums[min_ind])
+           min_ind=left;
+        if(right<n-leave&&nums[right]>nums[min_ind])
+           min_ind=right;
+
+        if(min_ind!=i)
+        {
+            int t= nums[min_ind];
+            nums[min_ind]=nums[i];
+            nums[i]=t;
+            heapify(nums, min_ind, leave);
         }      
     }
+
+
     vector<int> sortArray(vector<int>& nums) {
-        int n = nums.size();
+      int n = nums.size();
 
-        for(int i=n/2; i>=0; i--)
-        {
-            heapify(nums, i, 0);
-        }
+      for(int i=n/2; i>=0; i--)
+      {
+          heapify(nums, i, 0);
+      }    
 
-        for(int i=0; i<n-1; i++)
-        {
-            int t = nums[0];
-            nums[0]=nums[n-1-i];
-            nums[n-1-i]=t;
-            heapify(nums, 0, i+1);
-        }
-        return nums;
+      for(int i=0; i<n-1; i++)
+      {
+         int t= nums[n-1-i];
+         nums[n-1-i]=nums[0];
+         nums[0]=t;
+         heapify(nums, 0, i+1);
+      }
+      return nums;
     }
 };
