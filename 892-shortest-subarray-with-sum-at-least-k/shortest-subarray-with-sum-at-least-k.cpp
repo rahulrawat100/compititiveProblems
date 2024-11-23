f5d25cc9cdda23299d809207206f5c6a52767c50
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2024-11-23 10:33:22 +0530

    Time: 103 ms (9.63%) | Memory: 119.8 MB (12.23%) - LeetSync

--- 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.cpp
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.cpp
@@ -2,76 +2,21 @@ class Solution {
 public:
     int shortestSubarray(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<long long> prefix(n+1, 0);
-        int l=0;
-        while(l<n&&nums[l]<0)
-        {
-            l++;
-        }
-        for(int i=l; i<n; i++)
-        {
-            prefix[i+1]=prefix[i]+nums[i];
-        }
-
-        vector<int> nse(n+1, n+1);
-        stack<int> D;
-        for(int i=0; i<=n; i++)
-        {
-            while(!D.empty() && prefix[D.top()]>prefix[i])
-            {
-                nse[D.top()]=i;
-                D.pop();
-            }
-            D.push(i);
-        }
-        // for(int i=0; i<=n; i++)
-        // {
-        //     cout<<prefix[i]<<"   ";
-        // }
-        // cout<<endl;
-        // for(int i=0; i<=n; i++)
-        // {
-        //     cout<<nse[i]<<"   ";
-        // }
-        //  cout<<"1"<<endl;
-        int i=l;
-        int j=l;
+        priority_queue<pair<long long, int>> pq;
         int res=n+1;
-        while(i<n&&nums[i]<=0)
+        long long cum=0;
+        pq.push({0, -1});
+        for(int i=0; i<n; i++)
         {
-            i++;
-            j++;
-        }
-        // cout<<"2"<<endl;
-        while(j<n)
-        {
-            cout<<j<<endl;
-            while(j<n&&prefix[j]-prefix[i]<k)
-            {
-                j++;
-                if(nse[i]==j)
-                   break;
-            }
-            if(nse[i]==j)
-            {
-                i=j;
-                continue;
-            }
-            while(i<j&&(prefix[j]-prefix[i]>=k || nums[i]<=0))
-            {
-                if(prefix[j]-prefix[i]>=k)
-                   res=min(res, j-i);
-                i++;
-            }
+            cum+=nums[i];
+            pq.push({-cum, i});
 
-            while(nse[i]<j)
+            while(!pq.empty() && cum+pq.top().first>=k)
             {
-                i=nse[i];
-                if(prefix[j]-prefix[i]>=k)
-                   res=min(res, j-i);
+                res=min(res, i-pq.top().second);
+                pq.pop();
             }
         }
         return res==n+1?-1:res;
-        
     }
 };
