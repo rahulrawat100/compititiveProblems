fa9ba2310608a22a826b167681084c4d0a7fa432
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2025-01-10 22:19:04 +0530

    Time: 133 ms (5.28%) | Memory: 135.1 MB (6.02%) - LeetSync

---
+++ 2465-shifting-letters-ii/shifting-letters-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        unordered_map<int, int> mpi;
+        unordered_map<int, int> mpd;
+
+        int m =shifts.size();
+
+        for(int i=0; i<m ;i++)
+        {
+           if(shifts[i][2]==1)
+            {
+              mpi[shifts[i][0]]++;
+              mpi[shifts[i][0]]=mpi[shifts[i][0]];
+              mpi[shifts[i][1]+1]--;
+            }
+            else
+           {
+              mpd[shifts[i][0]]++;
+              mpd[shifts[i][1]+1]--;
+            }
+        }
+        
+        int curr=0;
+        for(int i=0; i<s.size(); i++)
+        {
+            int ind = s[i]-'a';
+            curr+=mpi[i];
+            ind+=curr;   
+            ind=(ind+260000)%26;
+            //cout<<ind<<endl;
+            s[i]=(char)(ind+'a');   
+        }
+
+        curr=0;
+        for(int i=0; i<s.size(); i++)
+        {
+            int ind = s[i]-'a';
+            curr+=mpd[i];
+            ind-=curr;   
+            ind=(ind+260000)%26;
+            //cout<<ind<<endl;
+            s[i]=(char)(ind+'a');   
+        }
+        return s;
+    }
+};
