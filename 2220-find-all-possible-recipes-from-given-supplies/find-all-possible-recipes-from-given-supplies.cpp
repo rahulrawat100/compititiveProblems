dd8369f75e12d088f03f29687a95b4815c6c306d
rahulrawat100 <105353725+rahulrawat100@users.noreply.github.com>
2025-03-21 16:48:21 +0530

    Time: 1746 ms (5.05%) | Memory: 121.1 MB (58.16%) - LeetSync

---
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -0,0 +1,85 @@
+class Trie
+{
+    public:
+    Trie* child[26];
+    bool ends = false;
+};
+class Solution {
+public:
+    Trie* root;
+    int count=0;
+    void Insert(string s)
+    {
+        Trie* temp = root;
+        for(int i=0; i<s.size(); i++)
+        {
+            int ind = s[i]-'a';
+            if(temp->child[ind]==NULL)
+               temp->child[ind]= new Trie();
+            temp = temp->child[ind];   
+        }
+        temp->ends= true;
+    }
+
+    bool Contains(string s)
+    {
+        Trie* temp = root;
+        for(int i=0; i<s.size(); i++)
+        {
+            int ind = s[i]-'a';
+            if(temp->child[ind]==NULL)
+               return false;
+            temp = temp->child[ind];   
+        }
+        return temp->ends;
+    }
+
+    bool Check(vector<vector<string>>& ingredients,unordered_map<string, int>& D, vector<string>& recipes, int i, vector<string>& res)
+    {
+        int j=0;
+        if(count>101)
+           return false;
+           count++;
+        while(j<ingredients[i].size())
+        {
+            if(D.find(ingredients[i][j])!=D.end())
+            {
+                if(!Check(ingredients, D, recipes, D[ingredients[i][j]], res))
+                   break; 
+            }
+            else if(!Contains(ingredients[i][j]))
+                break;
+             j++;
+        }
+            if(j==ingredients[i].size()&&!Contains(recipes[i]))
+            {
+               Insert(recipes[i]);
+               res.push_back(recipes[i]);
+            }
+            count--;
+            return j == ingredients[i].size();
+    }
+
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        root= new Trie();
+
+        for(int i=0; i<supplies.size(); i++)
+        {
+            Insert(supplies[i]);
+        }
+        unordered_map<string, int> D;
+        vector<string> res;
+        for(int i=0; i<recipes.size();i++)
+        {
+            D[recipes[i]]=i;
+        }
+        for(int i=0; i<recipes.size();i++)
+        {
+             Check(ingredients, D, recipes, i, res);
+        }
+
+        return  res;
+
+
+    }
+};
